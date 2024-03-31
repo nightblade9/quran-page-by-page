ca3095328a90be474479cc692a1a06c207a1a9a2
nightblade9 <nightbladecodes@gmail.com>
2024-03-31 02:51:02 -0400

    Fix: output should be Arabic characters, parsable JSON.

--- main.py
+++ main.py
@@ -31,16 +31,38 @@ class Main:
         pages.append(current_page)
 
         with open(Main._OUTPUT_FILE, 'w', encoding='utf-8') as file_handle:
-            file_handle.write('[')
+            file_handle.write('[') # top-level array
             
             for page in pages:
-                file_handle.write(json.dumps(page))
+                file_handle.write('[') # page start
+                quoted_lines = [f'"{x}"' for x in page]
+
+                for line in quoted_lines:
+                    file_handle.write(line)
+
+                    if not line == quoted_lines[len(quoted_lines) - 1]:
+                        file_handle.write(", ") # between lines
+                
+                file_handle.write(']') # page end
 
                 if not page == pages[len(pages) - 1]:
-                    file_handle.write(', ')
+                    file_handle.write(', ') # between pages
         
-            file_handle.write(']')
+            file_handle.write(']') # top-level array
         
         print(f"Done {len(pages)} pages; check out {Main._OUTPUT_FILE}.")
+
+        # FIX IT. We get double-double-quotes, ("") for some reason...
+        with open(Main._OUTPUT_FILE, 'r', encoding='utf-8') as file_handle:
+            raw = file_handle.read()
+        raw = raw.replace('""', '", "')
+        with open(Main._OUTPUT_FILE, 'w', encoding='utf-8') as file_handle:
+            file_handle.write(raw)
+
+        # TEST IT.
+        with open(Main._OUTPUT_FILE, 'r', encoding='utf-8') as file_handle:
+            data = json.load(file_handle)
+        print("Confirmed: file is parsable as JSON.")
+
 
 Main().run()
