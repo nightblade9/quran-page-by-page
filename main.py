ddf1eba8aa9d36816dccd56115d941c8206b8dca
nightblade9 <nightbladecodes@gmail.com>
2024-04-02 16:19:54 -0400

    Crude implementation of pages-per-surah.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import json
 class Main:
     _SOURCE_FILE = 'source.json'
     _OUTPUT_FILE = 'quran-pages.json'
+    _SURAHS_OUTPUT_FILE = 'surah-pages.json'
     
     def run(self):
         with open(Main._SOURCE_FILE, 'r', encoding='utf-8') as file_handle:
@@ -11,14 +12,22 @@ class Main:
 
         print(f"Parsing {len(data)} entries from our JSON array ...")
 
+        # Collect the text of each page
         pages = []
         # 604 entries. Each is an array of Qur'anic text.
         current_page = []
         current_page_number = 1
 
+        # Collect the range of pages for each surah
+        surahs = [] # 114 entries
+        current_surah = [] # list of pages, e.g. [2, 3, 4]
+        current_surah_number = 1
+
+        # Collect each page's data
         for ayah in data:
             arabic = ayah["uthmaniText"]
             page_number = ayah["page"]
+            surah_number = ayah["surah"]["number"]
 
             if page_number == current_page_number:
                 current_page.append(arabic)
@@ -26,10 +35,25 @@ class Main:
                 pages.append(current_page)
                 current_page = []
                 current_page_number = page_number
+            
+            if surah_number == current_surah_number:
+                if page_number not in current_surah:
+                    current_surah.append(page_number)
+            else:
+                surahs.append(current_surah)
+                current_surah = []
+                current_surah_number = surah_number
         
-        # Final ayah
+        # Final ayah goes on the final page
         pages.append(current_page)
+        # Final page goes in final surah
+        surahs.append(current_surah)
 
+        #self.write_list_of_pages(pages)
+        self.write_surah_pages_index(surahs)
+
+    def write_list_of_pages(self, pages):
+        # Write the output, taking care to quote it properly
         with open(Main._OUTPUT_FILE, 'w', encoding='utf-8') as file_handle:
             file_handle.write('[') # top-level array
             
@@ -52,17 +76,28 @@ class Main:
         
         print(f"Done {len(pages)} pages; check out {Main._OUTPUT_FILE}.")
 
-        # FIX IT. We get double-double-quotes, ("") for some reason...
+        # Massage the final output to remove any JSON errors.
+        # Fix a bug where we get double-double-quotes, ("") for some reason...
         with open(Main._OUTPUT_FILE, 'r', encoding='utf-8') as file_handle:
             raw = file_handle.read()
         raw = raw.replace('""', '", "')
         with open(Main._OUTPUT_FILE, 'w', encoding='utf-8') as file_handle:
             file_handle.write(raw)
 
-        # TEST IT.
+        # TEST IT: load it back and verify that we can parse it (valid JSON)
         with open(Main._OUTPUT_FILE, 'r', encoding='utf-8') as file_handle:
             data = json.load(file_handle)
         print("Confirmed: file is parsable as JSON.")
 
+    def write_surah_pages_index(self, surahs):
+         with open(Main._SURAHS_OUTPUT_FILE, 'w') as file_handle:
+            file_handle.write('[') # top-level array
+
+            for surah in surahs:
+                file_handle.write(json.dumps(surah))
+                if not surah == surahs[len(surahs) - 1]:
+                    file_handle.write(', ') # between surahs
+
+            file_handle.write(']') # top-level array
 
 Main().run()
